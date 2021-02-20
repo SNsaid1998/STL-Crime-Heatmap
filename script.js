cf801d71b2f9d2df881f3e33bdf610e2e027a52d
SNsaid1998 <61479527+SNsaid1998@users.noreply.github.com>
2021-02-19 18:37:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -21,33 +21,31 @@ require([
           title: "St. Louis Crime Heatmap",
           content: "Crime {Crime} {type} Neighboorhood {Neighborhood} on {ILEADSStreet}."
         };
-      
-const layer = new CSVLayer({
-        url: url,
-        title: "St. Louis Crime Heatmap",
-        copyright: "St. Louis Police Department",
-		latitudeField:"Lat",
+
+        const csvLayer = new CSVLayer({
+          url: url,
+          copyright: "St. Louis Police Department",
+          latitudeField:"Lat",
         longitudeField:"Lon",
-		popupTemplate: template,
-		renderer: renderer
-});
+          popupTemplate: template
+        });
 
       csvLayer.renderer = {
         type: "heatmap", 
         colorStops: [
             { color: "rgba(63, 40, 102, 0)", ratio: 0 },
-            { color: "#191970", ratio: 0.083 },
-            { color: "#0000cd", ratio: 0.166 },
-            { color: "#1e90ff",ratio: 0.249 },
-            { color: "#00bfff", ratio: 0.332 },
-            { color: "#ff6347", ratio: 0.415 },
-            { color: "#ff4500", ratio: 0.498 },
-            { color: "#ff0000", ratio: 0.581 },
-            { color: "#c71585", ratio: 0.664 },
-            { color: "#ff00ff", ratio: 0.747 },
-            { color: "#ff1493", ratio: 0.83 },
-            { color: "#ffd700", ratio: 0.913 },
-            { color: "#adff2f", ratio: 1 }
+            { color: "#9C33FF", ratio: 0.083 },
+            { color: "#6B33FF", ratio: 0.166 },
+            { color: "#3386FF",ratio: 0.249 },
+            { color: "#33CAFF", ratio: 0.332 },
+            { color: "#33FFEC", ratio: 0.415 },
+            { color: "#33FFAF", ratio: 0.498 },
+            { color: "#42FF33", ratio: 0.581 },
+            { color: "#96FF33", ratio: 0.664 },
+            { color: "#E0FF33", ratio: 0.747 },
+            { color: "#FFE633", ratio: 0.83 },
+            { color: "#FFA233", ratio: 0.913 },
+            { color: "#FF3333", ratio: 1 }
           ],
           maxPixelIntensity: 400,
           minPixelIntensity: 0
