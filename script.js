269d30c17558756b747e86de6e313e223da5b842
SNsaid1998 <61479527+SNsaid1998@users.noreply.github.com>
2021-02-19 18:59:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-    require([
+require([
       "esri/Map",
       "esri/layers/CSVLayer",
       "esri/views/MapView",
@@ -12,40 +12,53 @@
       esriConfig,
       urlUtils
     ) {
-      
 
-var url = "https://raw.githubusercontent.com/gbrunner/Advanced_Python_for_GIS_and_RS/master/Week%202/stl_crime_wgs_84.csv";
+     var url = "https://raw.githubusercontent.com/gbrunner/Advanced_Python_for_GIS_and_RS/master/Week%202/stl_crime_wgs_84.csv";
      esriConfig.request.corsEnabledServers.push('https://rawgit.com');
 
+ 
         const template = {
-          title: "Saint Louis City Crime CSV Layer",
-          content: "Feature Layer, Crime CSV"
+          title: "St. Louis Crime Heatmap",
+          content: "Crime {Crime} {type} Neighboorhood {Neighborhood} on {ILEADSStreet}."
         };
 
         const csvLayer = new CSVLayer({
           url: url,
-          copyright: "STL CRIME CSV",
+          copyright: "St. Louis Police Department",
+          latitudeField:"Lat",
+        longitudeField:"Lon",
           popupTemplate: template
         });
 
-        var symbol = {
-          type: "simple-marker", 
-          color:"red"
-        };
-
       csvLayer.renderer = {
-        type: "simple", // autocasts as new SimpleRenderer()
-        symbol: symbol
+        type: "heatmap", 
+        colorStops: [
+            { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+            { color: "#9C33FF", ratio: 0.083 },
+            { color: "#6B33FF", ratio: 0.166 },
+            { color: "#3386FF",ratio: 0.249 },
+            { color: "#33CAFF", ratio: 0.332 },
+            { color: "#33FFEC", ratio: 0.415 },
+            { color: "#33FFAF", ratio: 0.498 },
+            { color: "#42FF33", ratio: 0.581 },
+            { color: "#96FF33", ratio: 0.664 },
+            { color: "#E0FF33", ratio: 0.747 },
+            { color: "#FFE633", ratio: 0.83 },
+            { color: "#FFA233", ratio: 0.913 },
+            { color: "#FF3333", ratio: 1 }
+          ],
+          maxPixelIntensity: 400,
+          minPixelIntensity: 0
       };
 
       var map = new Map({
-        basemap: "gray",
+        basemap: "dark-gray",
         layers: [csvLayer]
       });
 
       var view = new MapView({
         container: "viewDiv",
-        center: [-90.25, 38.65],
+        center: [-90.1994, 38.6270],
         zoom: 12,
         map: map
       });
